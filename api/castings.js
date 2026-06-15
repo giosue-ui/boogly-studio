ddb19e58d0522bc4b2455d23a6b5a9951bdc6b75
giosue-ui <giosuesbusiness@gmail.com>
2026-06-15 20:44:15 +0200

    Create castings.js

---
+++ api/castings.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+
+  const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+  const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+
+  try {
+    const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/Castings?filterByFormula=%7BAktiv%7D%3D1&sort%5B0%5D%5Bfield%5D=Reihenfolge&sort%5B0%5D%5Bdirection%5D=asc`;
+
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `Bearer ${AIRTABLE_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) throw new Error('Airtable error: ' + response.status);
+
+    const data = await response.json();
+    const castings = data.records.map(r => r.fields.Name).filter(Boolean);
+
+    res.status(200).json({ castings });
+  } catch (err) {
+    res.status(200).json({ castings: [], error: err.message });
+  }
+}
